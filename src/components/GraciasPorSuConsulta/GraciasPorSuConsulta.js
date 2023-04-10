a140c135f13ebee40e5e2cab50609a021f59752c
yo <esteban.altamura@gmail.com>
2023-04-10 15:08:37 -0300

    jo

--- src/components/GraciasPorSuConsulta/GraciasPorSuConsulta.js
+++ src/components/GraciasPorSuConsulta/GraciasPorSuConsulta.js
@@ -24,7 +24,10 @@ export const GraciasPorSuConsulta = ()=> {
             
             <div ref={mensajeGraciasContainer} className="mensajeGraciasContainer">
             <Header />
+                <h4 className="loConactaremosmessage">En menos de 24 horas nos contactaremos con usted</h4>
+                
                 <div className="buttonsContainer">
+                    <h5 className="ganchoChatMessage">Si quiere puede hablar ya mismo con un representante</h5>
                     <Link to="https://wa.me/+5491127704684?text=Hola!%20Estoy%20interesado%20en%20el%20servicio,%20por%20favor%20contactarse%20a%20la%20brevedad." className="chatButton">
                         Chatear ahora
                         <img className="chatAhoraIcon" src="https://i.postimg.cc/sgz0nSHy/icons8-whatsapp-96.png"/>
