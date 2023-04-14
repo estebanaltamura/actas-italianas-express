4e0da5c33bab1d2c48e2969ec58af9b63f568748
yo <esteban.altamura@gmail.com>
2023-04-14 11:06:22 -0300

    hoi

--- src/components/FormularioWhatsapp/FormularioWhatsapp.js
+++ src/components/FormularioWhatsapp/FormularioWhatsapp.js
@@ -1,6 +1,6 @@
 import "./FormularioWhatsapp.css"
 import { FormWhatsapp } from "../Form/FormWhatsapp"
-import { Footer } from "../Footer/Footer"
+import { FooterGracias } from "../Footer/FooterGracias"
 import { Header } from "../Header/Header"
 
 export const FormularioWhatsapp = ()=>{
@@ -11,7 +11,7 @@ export const FormularioWhatsapp = ()=>{
                 <h4 className="chatearPageMessage">Ingresa tu informacion de contacto para iniciar la asesoria gratis por whatsapp</h4>
                 <FormWhatsapp />
             </div>
-            <Footer />
+            <FooterGracias />
         </>
     )
 }
