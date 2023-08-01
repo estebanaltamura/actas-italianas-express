4b781aa90c9eebe8a23a2eabfee0770e6ce5826c
estebanaltamura <esteban.altamura@gmail.com>
2023-08-01 10:18:19 -0300

    ac

--- src/pages/GraciasPorSuConsulta/GraciasPorSuConsulta.js
+++ src/pages/GraciasPorSuConsulta/GraciasPorSuConsulta.js
@@ -23,7 +23,7 @@ export const GraciasPorSuConsulta = () => {
         </h4>
         <div className="buttonsContainer">
           <h5 className="ganchoChatMessage">
-            Si quiere puede hablar ya mismo con un representante
+            Si quiere puede hablar ya mismo con un representante    
           </h5>
           <a
             href="https://wa.me/+5491127704684?text=Hola!%20Estoy%20interesado%20en%20el%20servicio,%20por%20favor%20contactarse%20a%20la%20brevedad."
