650714129aaf4becf9579d775d3b1c40746f8790
estebanaltamura <esteban.altamura@gmail.com>
2023-11-16 08:09:28 -0300

    actualizacion

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,11 @@
-import { useState, useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
-import { useFormValidator } from "../../hooks/useFormValidator";
-import { usePhoneNumberHandler } from "../../hooks/usePhoneNumberHandler";
-import { useSubmitLeadToFirestore } from "../../hooks/useSubmitLeadToFirestore";
-import { usePopUps } from "../../hooks/usePopUps";
-import { TbHelp } from "react-icons/tb";
-import "./Form.css";
+import { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useFormValidator } from '../../hooks/useFormValidator';
+import { usePhoneNumberHandler } from '../../hooks/usePhoneNumberHandler';
+import { useSubmitLeadToFirestore } from '../../hooks/useSubmitLeadToFirestore';
+import { usePopUps } from '../../hooks/usePopUps';
+import { TbHelp } from 'react-icons/tb';
+import './Form.css';
 
 export const Form = () => {
   const {
@@ -20,7 +20,7 @@ export const Form = () => {
   const { submitForm } = useSubmitLeadToFirestore();
   const { phoneNumberFormHelperPopUp } = usePopUps();
 
-  const [ section, setSection ] = useState(null)
+  const [section, setSection] = useState(null);
 
   const fullNameValueInput = useRef();
   const phoneValueInput = useRef();
@@ -38,7 +38,7 @@ export const Form = () => {
 
     const fullName = e.target.fullName.value.toLowerCase();
     const phoneNumber = e.target.phone.value;
-    const mail = e.target.mail.value.toLowerCase(); 
+    const mail = e.target.mail.value.toLowerCase();
 
     const phoneNumberHandled = phoneNumberHandler(phoneNumber);
 
@@ -49,79 +49,86 @@ export const Form = () => {
         mailElement: mailValueInput.current,
         submitButton: submit.current,
       };
-      
-      submitForm(fullName, phoneNumberHandled, mail, elements, section);    
+
+      submitForm(fullName, phoneNumberHandled, mail, elements, section);
     } else setAlerts(fullName, phoneNumberHandled, mail);
   };
 
-  useEffect(()=>{
-    const urlInParts = url.pathname.split("/")
-    const section = urlInParts[1]
-    setSection(section)
-  },[url])
+  useEffect(() => {
+    const urlInParts = url.pathname.split('/');
+    const section = urlInParts[1];
+    setSection(section);
+  }, [url]);
 
   return (
     <>
-      <div className="formWhatsappContainer">
+      <div className='formWhatsappContainer'>
         <form
-          className="formFormWhatsapp"
-          action="/action_page.php"
+          className='formFormWhatsapp'
+          action='/action_page.php'
           onSubmit={onSubmitHandler}
         >
-          <div className="inputContainer">
-            <span className="subtitle">NOMBRE:</span>
+          <div className='inputContainer'>
+            <span className='subtitle'>NOMBRE:</span>
             <input
-              placeholder="Ingrese su nombre completo"
-              className="inputformWhatsapp"
+              placeholder='Ingrese su nombre completo'
+              className='inputformWhatsapp'
               ref={fullNameValueInput}
-              autoComplete="off"
-              type="text"
-              name="fullName"
+              autoComplete='off'
+              type='text'
+              name='fullName'
               onKeyUp={resetAlerts}
             />
-            <span className="inputAlerts">{fullNameAlert}</span>
+            <span className='inputAlerts'>{fullNameAlert}</span>
           </div>
 
-          <div className="inputContainer">
-            <span className="subtitle">WHATSAPP:</span>
+          <div className='inputContainer'>
+            <span className='subtitle'>WHATSAPP:</span>
             <input
-              placeholder="Ingrese su Whatsapp Ejemplo 113 859 7894"
-              className="inputformWhatsapp"
+              placeholder='Ingrese su Whatsapp Ejemplo 113 859 7894'
+              className='inputformWhatsapp'
               ref={phoneValueInput}
-              autoComplete="off"
-              type="number"
-              name="phone"
+              autoComplete='off'
+              type='number'
+              name='phone'
               onKeyUp={resetAlerts}
             />
             <TbHelp
               onClick={inputPhoneHelpClickHandler}
-              className="inputPhoneHelp"
+              className='inputPhoneHelp'
             />
-            <span className="inputAlerts">{phoneAlert}</span>
+            <span className='inputAlerts'>{phoneAlert}</span>
           </div>
 
-          <div className="inputContainer">
-            <span className="subtitle">MAIL:</span>
+          <div className='inputContainer'>
+            <span className='subtitle'>MAIL:</span>
             <input
-              placeholder="Ingrese su e-mail Ejemplo mail@mail.com"
-              className="inputformWhatsapp"
+              placeholder='Ingrese su e-mail Ejemplo mail@mail.com'
+              className='inputformWhatsapp'
               ref={mailValueInput}
-              autoComplete="off"
-              type="text"
-              name="mail"
+              autoComplete='off'
+              type='text'
+              name='mail'
               onKeyUp={resetAlerts}
             />
-            <span className="inputAlerts">{mailAlert}</span>
+            <span className='inputAlerts'>{mailAlert}</span>
           </div>
-          <button ref={submit} type="submit" className={section === "chatearConUnOperador" ? "chatButtonChatear" : "submitButtonFormHome"}>
-            {
-              section === "chatearConUnOperador" &&
-                <img
-                  className="iconWhatsappChatear"
-                  src="https://i.postimg.cc/sgz0nSHy/icons8-whatsapp-96.png"
-                />
-            }            
-            {section === "chatearConUnOperador" ? "CHATEA AHORA" : "ENVIAR"} 
+          <button
+            ref={submit}
+            type='submit'
+            className={
+              section === 'chatearConUnOperador'
+                ? 'chatButtonChatear'
+                : 'submitButtonFormHome'
+            }
+          >
+            {section === 'chatearConUnOperador' && (
+              <img
+                className='iconWhatsappChatear'
+                src='https://i.postimg.cc/sgz0nSHy/icons8-whatsapp-96.png'
+              />
+            )}
+            {section === 'chatearConUnOperador' ? 'CHATEA AHORA' : 'ENVIAR'}
           </button>
         </form>
       </div>
--- src/hooks/useSubmitLeadToFirestore.js
+++ src/hooks/useSubmitLeadToFirestore.js
@@ -1,110 +1,154 @@
-import { useNavigate } from "react-router-dom";
-import { usePopUps } from "./usePopUps";
+import { useNavigate } from 'react-router-dom';
+import { usePopUps } from './usePopUps';
 import {
-  getFirestore,  
+  getFirestore,
   setDoc,
   doc,
   getDoc,
   updateDoc,
-} from "firebase/firestore";
+} from 'firebase/firestore';
 
+export const useSubmitLeadToFirestore = () => {
+  const db = getFirestore();
+  const history = useNavigate();
 
-export const useSubmitLeadToFirestore = ()=>{
-  const db = getFirestore();  
-  const history = useNavigate()
+  const { submitFormError } = usePopUps();
 
-  const { submitFormError } = usePopUps()
-  
-  const date = new Date().toLocaleDateString("en-us", {
-      weekday: "long",
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });   
+  const date = new Date().toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
-    const elementsBehaviorWhenSubmit = (fullNameELement, phoneNumberElement, mailElement, submitButton)=>{
-      submitButton.setAttribute("disabled", "true");
-      submitButton.style.backgroundColor = "#63BEE6";
-      submitButton.textContent = "ENVIANDO...";      
-      fullNameELement.value = "";
-      phoneNumberElement.value = "";
-      mailElement.value = "";      
-    }
+  const elementsBehaviorWhenSubmit = (
+    fullNameELement,
+    phoneNumberElement,
+    mailElement,
+    submitButton
+  ) => {
+    submitButton.setAttribute('disabled', 'true');
+    submitButton.style.backgroundColor = '#63BEE6';
+    submitButton.textContent = 'ENVIANDO...';
+    fullNameELement.value = '';
+    phoneNumberElement.value = '';
+    mailElement.value = '';
+  };
 
-    const getLastLeadIdNumber = async()=>{
-      const queryDoc = doc(
-        db,
-        "UltimoNumeroDeContacto",
-        "E9mEVykIO4N7719I6dh3"
-      );
-      const lastLeadIdNumberesponse = await getDoc(queryDoc);
-      const lastLeadIdNumber = lastLeadIdNumberesponse.data().ultimoNumero;
-      return lastLeadIdNumber
-    }
+  const elementsBehaviorWhenSubmitIsuccessful = (submitButton) => {
+    submitButton.setAttribute('disabled', 'false');
+    submitButton.style.backgroundColor = '#384d56';
+    submitButton.textContent = 'ENVIADO!';
+  };
 
-    const updateLastLeadIdNumberInFirestore = async(lastLeadIdNumber)=>{
-      const queryDoc = doc(
-        db,
-        "UltimoNumeroDeContacto",
-        "E9mEVykIO4N7719I6dh3"
-      );
-      const lastLeadIdNumberPlusOne = lastLeadIdNumber + 1;
-      await updateDoc(queryDoc, { ultimoNumero: lastLeadIdNumberPlusOne });
-      return lastLeadIdNumberPlusOne
-    }
+  const getLastLeadIdNumber = async () => {
+    const queryDoc = doc(db, 'UltimoNumeroDeContacto', 'E9mEVykIO4N7719I6dh3');
+    const lastLeadIdNumberesponse = await getDoc(queryDoc);
+    const lastLeadIdNumber = lastLeadIdNumberesponse.data().ultimoNumero;
+    return lastLeadIdNumber;
+  };
 
-    const createDocument = (lastLeadIdNumberPlusOne, fullName)=>{
-      const docRef = doc(
-        db,
-        "LeadsParaAgendar",
-        `${lastLeadIdNumberPlusOne} - ${fullName}`
-      );
+  const updateLastLeadIdNumberInFirestore = async (lastLeadIdNumber) => {
+    const queryDoc = doc(db, 'UltimoNumeroDeContacto', 'E9mEVykIO4N7719I6dh3');
+    const lastLeadIdNumberPlusOne = lastLeadIdNumber + 1;
+    await updateDoc(queryDoc, { ultimoNumero: lastLeadIdNumberPlusOne });
+    return lastLeadIdNumberPlusOne;
+  };
 
-      return docRef
-    }
+  const createDocument = (lastLeadIdNumberPlusOne, fullName) => {
+    const docRef = doc(
+      db,
+      'LeadsParaAgendar',
+      `${lastLeadIdNumberPlusOne} - ${fullName}`
+    );
 
-    const setDocument = (docRef, lastLeadIdNumberPlusOne, date, fullName, phoneNumberHandled, mail, submitButton, section)=>{      
-      setDoc(docRef, {
+    return docRef;
+  };
+
+  const setDocument = (
+    docRef,
+    lastLeadIdNumberPlusOne,
+    date,
+    fullName,
+    phoneNumberHandled,
+    mail,
+    submitButton,
+    section
+  ) => {
+    setDoc(docRef, {
       fullname: `${lastLeadIdNumberPlusOne} - ${fullName}`,
       phoneNumber: phoneNumberHandled,
       email: mail,
       date: date,
       cumplimentada: false,
-      })
-      .then(() => {         
-        if(section === "home"){
-          history('/graciasPorSuConsulta') 
-        } 
-        if(section === "chatearConUnOperador"){
-          const timeOut = setTimeout(()=>{
-            history('/home') 
-            clearTimeout(timeOut)
-          }, 3000)
-  
-          window.open('https://wa.me/+5491127704684?text=Hola!%20Estoy%20interesado%20en%20el%20servicio,%20por%20favor%20contactarse%20a%20la%20brevedad.', '_blank' )
-        }                    
+    })
+      .then(() => {
+        if (section === 'home') {
+          history('/graciasPorSuConsulta');
+        }
+        if (section === 'chatearConUnOperador') {
+          const timeOut = setTimeout(() => {
+            history('/home');
+            clearTimeout(timeOut);
+          }, 3000);
+
+          window.open(
+            'https://wa.me/+5491127704684?text=Hola!%20Estoy%20interesado%20en%20el%20servicio,%20por%20favor%20contactarse%20a%20la%20brevedad.',
+            '_blank'
+          );
+        }
       })
       .catch((error) => {
         console.log(error);
-        submitButton.setAttribute("disabled", "false");
-        submitButton.style.backgroundColor = "#384d56";
-        submitButton.textContent = "CHATEA AHORA";      
+        submitButton.setAttribute('disabled', 'false');
+        submitButton.style.backgroundColor = '#384d56';
+        submitButton.textContent = 'CHATEA AHORA';
         submitFormError();
       });
-    }
+  };
 
-    const submitForm = async(fullName, phoneNumberHandled, mail, elements, section)=>{      
-      const { fullNameELement, phoneNumberElement, mailElement, submitButton } = elements       
-      elementsBehaviorWhenSubmit(fullNameELement, phoneNumberElement, mailElement, submitButton)      
-      const lastLeadIdNumber = await getLastLeadIdNumber()
-      const lastLeadIdNumberPlusOne = await updateLastLeadIdNumberInFirestore(lastLeadIdNumber)
-      const docRef = createDocument(lastLeadIdNumberPlusOne, fullName)
-      setDocument(docRef, lastLeadIdNumberPlusOne, date, fullName, phoneNumberHandled, mail, submitButton, section)      
-    }   
+  const submitForm = async (
+    fullName,
+    phoneNumberHandled,
+    mail,
+    elements,
+    section
+  ) => {
+    const { fullNameELement, phoneNumberElement, mailElement, submitButton } =
+      elements;
+    elementsBehaviorWhenSubmit(
+      fullNameELement,
+      phoneNumberElement,
+      mailElement,
+      submitButton
+    );
+    try {
+      const lastLeadIdNumber = await getLastLeadIdNumber();
+      const lastLeadIdNumberPlusOne = await updateLastLeadIdNumberInFirestore(
+        lastLeadIdNumber
+      );
+      const docRef = createDocument(lastLeadIdNumberPlusOne, fullName);
+      setDocument(
+        docRef,
+        lastLeadIdNumberPlusOne,
+        date,
+        fullName,
+        phoneNumberHandled,
+        mail,
+        submitButton,
+        section
+      );
+      elementsBehaviorWhenSubmitIsuccessful(submitButton);
+      const timeOut = setTimeout(() => {
+        submitButton.textContent = 'ENVIAR';
+        clearTimeout(timeOut);
+      }, 1500);
+    } catch (error) {}
+  };
 
-  return({
-    submitForm
-  })
-}
+  return {
+    submitForm,
+  };
+};
